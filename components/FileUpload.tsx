2e4d181eda1255ad07871bd2f61cea3f92eda6be
Greg <gjmooney@asu.edu>
2023-09-11 17:40:01 +0200

    More uploadthing stuff

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -2,6 +2,8 @@
 
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
+import { X } from "lucide-react";
+import Image from "next/image";
 
 interface FileUploadProps {
   value: string;
@@ -10,6 +12,25 @@ interface FileUploadProps {
 }
 
 const FileUpload = ({ value, endpoint, onChange }: FileUploadProps) => {
+  // Checking the filename
+  const fileType = value?.split(".").pop();
+
+  // checking if it's an image
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="absolute right-0 top-0 rounded-full bg-background p-1 shadow-sm"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  // TODO: style dropzone
   return (
     <UploadDropzone
       endpoint={endpoint}
